65df3e24b2d9cd259257f4394f824ef8e3f8f524
janakiputheth <puthethj@gmail.com>
2026-03-30 19:35:03 -0700

    Auth Changes
    Changes:
    - Added the new ui forms for register and login
    - Editted the seed.js file to use userService to fill database with temp users that have hashed password

--- book-buddy/src/App.jsx
+++ book-buddy/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddNewBookForm from "./components/AddNewBook/AddNewBookForm.jsx";
-import Register from "./pages/Register/Register.jsx";
-import LoginPage from "./pages/Login/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage/RegisterPage.jsx";
+import LoginPage from "./pages/LoginPage/LoginPage.jsx";
 import YourLibraryPage from "./pages/YourLibrary/YourLibraryPage.jsx";
 import BookDetails from "./pages/BookDetails/BookDetails.jsx";
 import DiscoverPage from "./pages/DiscoverPage/DiscoverPage.jsx";
@@ -13,8 +13,7 @@ function AppContent() {
         <Route path="/" element={<DiscoverPage />} />
         {/* <Route path="/discover" element={<Navigate to="/" replace />} /> */}
         <Route path="/add-book" element={<AddNewBookForm />} />
-        <Route path="/auth" element={<Register />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/library" element={<YourLibraryPage />} />
         <Route path="/book-details" element={<BookDetails />} />
--- book-buddy/src/pages/Register/Register.jsx
+++ book-buddy/src/pages/Register/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../../api.js";
-import "./Register.css";
+// import "./Register.css";
 
 function Register() {
   const navigate = useNavigate();
