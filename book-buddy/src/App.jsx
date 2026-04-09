60af0789b29df174550abb536281ea5cce99eea2
janakiputheth <puthethj@gmail.com>
2026-04-08 17:42:54 -0700

    Added removed ProfilePage to app.jsx

--- book-buddy/src/App.jsx
+++ book-buddy/src/App.jsx
@@ -10,6 +10,7 @@ import EditBookPage from "./pages/EditBookPage/EditBookPage.jsx";
 import LibraryPage from "./pages/LibraryPage/LibraryPage.jsx";
 import LoginPage from "./pages/LoginPage/LoginPage.jsx";
 import MessagesPage from "./pages/MessagesPage/MessagesPage.jsx";
+import ProfilePage from "./pages/ProfilePage/ProfilePage.jsx";
 import RegisterPage from "./pages/RegisterPage/RegisterPage.jsx";
 import RequestPage from "./pages/RequestPage/RequestPage.jsx";
 import SearchResultsPage from "./pages/SearchResultsPage/SearchResultsPage.jsx";
@@ -20,16 +21,17 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<DiscoverPage />} />
-        <Route path="/search" element={<SearchResultsPage />} />
-        <Route path="/library/edit/:bookId" element={<EditBookPage />} />
         <Route path="/add-book" element={<AddBookPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/requests" element={<RequestPage />} />
+        <Route path="/blogs" element={<BlogsPage />} />
+        <Route path="/book/:bookId" element={<BookDetailPage />} />
+        <Route path="/library/edit/:bookId" element={<EditBookPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/library" element={<LibraryPage />} />
-        <Route path="/book/:bookId" element={<BookDetailPage />} />
         <Route path="/messages" element={<MessagesPage />} />
-        <Route path="/blogs" element={<BlogsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/requests" element={<RequestPage />} />
+        <Route path="/search" element={<SearchResultsPage />} />
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<AdminDashboardPage />} />
           <Route path="users" element={<AdminUsersPage />} />
